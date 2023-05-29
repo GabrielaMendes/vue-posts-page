8e22a712072d8ea8944472e00b68d7a47e116b7d
Gabriela <gdemelo.mendes@gmail.com>
2023-05-29 17:36:08 -0300

    feature: created some basic posts and rendered them

--- src/components/TheTimeline.vue
+++ src/components/TheTimeline.vue
@@ -1,28 +1,44 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
+import { Post, today, thisWeek, thisMonth } from "../posts";
 
 const periods = ["Today", "This Week", "This Month"] as const;
 
-type Period = typeof periods[number]
+type Period = (typeof periods)[number];
 
 const selectedPeriod = ref<Period>("Today");
 
-function selectPeriod (period: Period) {
-  selectedPeriod.value = period;
+function selectPeriod(period: Period) {
+	selectedPeriod.value = period;
 }
+
+const posts: Post[] = [
+  today,
+  thisWeek,
+  thisMonth
+];
 </script>
 
 <template>
 	<nav class="is-primary panel">
 		<span class="panel-tabs">
-			<a 
-        v-for="period in periods"
-        :key="period"
-        :class="{ 'is-active': period === selectedPeriod }"
-        @click="selectPeriod(period)"
-      >
+			<a
+				v-for="period in periods"
+				:key="period"
+				:class="{ 'is-active': period === selectedPeriod }"
+				@click="selectPeriod(period)"
+			>
 				{{ period }}
 			</a>
 		</span>
+
+    <a 
+      v-for="post in posts"
+      :key="post.id"
+      class="panel-block"
+    >
+      <a>{{ post.title }}</a>
+      <div>{{ post.created }}</div>
+    </a>
 	</nav>
 </template>
