4ffc51a3a9ee98532dfd4101f8f31ebb17b7964e
Gabriela <gdemelo.mendes@gmail.com>
2023-05-30 18:24:39 -0300

    feature: handled input event to reflect markdown content

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,38 +1,46 @@
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
-import { TimelinePost } from '../posts';
+import { ref, onMounted } from "vue";
+import { TimelinePost } from "../posts";
 
 const props = defineProps<{
-  post: TimelinePost
-}>()
+	post: TimelinePost;
+}>();
 
-const title = ref(props.post.title)
-const content = ref(props.post.markdown)
-const contentEditable = ref<HTMLDivElement>()
+const title = ref(props.post.title);
+const content = ref(props.post.markdown);
+const contentEditable = ref<HTMLDivElement>();
 
 onMounted(() => {
-  console.log(contentEditable.value?.innerText)
-})
+	if (!contentEditable.value) {
+		throw Error("ContentEditable DOM node was not found");
+	}
+	contentEditable.value.innerText = content.value;
+});
+
+function handleInput() {
+	if (!contentEditable.value) {
+		throw Error("ContentEditable DOM node was not found");
+	}
+	content.value = contentEditable.value.innerText;
+}
 </script>
 
 <template>
-  <div class="columns">
-    <div class="column">
-      <div class="field">
-        <div class="label">Post title</div>
-        <input type="text" class="input" v-model="title">
-      </div>
-    </div>
-  </div>
+	<div class="columns">
+		<div class="column">
+			<div class="field">
+				<div class="label">Post title</div>
+				<input type="text" class="input" v-model="title" />
+			</div>
+		</div>
+	</div>
 
-  <div class="columns">
-      <div class="column">
-        <div contenteditable ref="contentEditable">
-          {{ content }}
-        </div>
-      </div>
-      <div class="column">
-        Preview
-      </div>
-    </div>
+	<div class="columns">
+		<div class="column">
+			<div contenteditable ref="contentEditable" @input="handleInput" />
+		</div>
+		<div class="column">
+			{{ content }}
+		</div>
+	</div>
 </template>
