948861e90b6e88b6f6e49487c798617ab272de7b
Gabriela <gdemelo.mendes@gmail.com>
2023-05-30 16:47:56 -0300

    feature: created the  home page and it's route and added RouterView to App

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router"
+import HomeView from "./views/HomeView.vue"
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: []
+  routes: [
+    {
+      path: "/",
+      component: HomeView,
+    }
+  ]
 })
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TheTimeline from "../components/TheTimeline.vue";
+</script>
+
+<template>
+	<Suspense>
+		<template #default>
+			<TheTimeline />
+		</template>
+
+		<template #fallback>
+			<progress class="progress is-primary is-small" />
+		</template>
+	</Suspense>
+</template>
