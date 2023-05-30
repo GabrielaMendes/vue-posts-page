7684371b8d607ea4483e6a0b392cfc4ed61f5b79
Gabriela <gdemelo.mendes@gmail.com>
2023-05-30 17:33:42 -0300

    feature: created component to write and edit posts

--- src/views/NewPostView.vue
+++ src/views/NewPostView.vue
@@ -1,3 +1,16 @@
+<script lang="ts" setup>
+import { DateTime } from "luxon";
+import { TimelinePost } from "../posts";
+import PostWriter from "../components/PostWriter.vue";
+
+const post: TimelinePost = {
+	id: "-1",
+	title: "Title",
+	created: DateTime.now(),
+};
+</script>
+
 <template>
-  New Post
+	New Post
+	<PostWriter :post="post" />
 </template>
