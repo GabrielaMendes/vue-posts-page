cdb83485aa4e74147ada98e009da8d59d456f671
Gabriela <gdemelo.mendes@gmail.com>
2023-05-31 18:09:14 -0300

    feature: created basic html for the modal and some functioning buttons to display it or hide

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <script lang="ts" setup>
-import TheNavbar from './components/TheNavbar.vue';
+import { computed } from "vue";
+import TheNavbar from "./components/TheNavbar.vue";
+import { useModal } from "./composables/modal";
+
+const modal = useModal();
+const modalVisibility = computed(() => {
+	return {
+		display: modal.show.value ? "block" : "none",
+	};
+});
 </script>
 
 <template>
+	<div class="modal" style="color: white" :style="modalVisibility">
+		<div class="modal-background">
+			<div class="modal-content">
+				<div id="modal"></div>
+			</div>
+			<button
+				class="modal-close is-large"
+				aria-label="close"
+				@click="modal.hideModal()"
+			></button>
+		</div>
+	</div>
+
 	<div class="section">
 		<div class="container">
-      <TheNavbar />
-      <RouterView />
+			<TheNavbar />
+			<RouterView />
 		</div>
 	</div>
 </template>
@@ -16,24 +38,29 @@ import TheNavbar from './components/TheNavbar.vue';
 @import "highlight.js/styles/atom-one-dark.css";
 
 ul {
-  list-style: revert !important;
-  list-style-position: inside !important;
+	list-style: revert !important;
+	list-style-position: inside !important;
 }
 
-h1, h2, h3, h4, h5, h6 {
-  font-size: revert !important;
-  margin: 10px 0 !important;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+	font-size: revert !important;
+	margin: 10px 0 !important;
 }
 
 pre {
-  margin: 10px 0 !important;
+	margin: 10px 0 !important;
 }
 
 p {
-  margin: 10px 0 !important;
+	margin: 10px 0 !important;
 }
 
 div[contenteditable] {
-  white-space: pre;
+	white-space: pre;
 }
 </style>
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,11 +1,22 @@
+<script lang="ts" setup>
+import { useModal } from "../composables/modal";
+
+const modal = useModal()
+</script>
+
 <template>
 	<div class="navbar">
 		<div class="navbar-end">
 			<div class="buttons">
+        <button class="button" @click="modal.showModal()">Sigh Up</button>
 				<RouterLink :to="{name: 'new'}" class="button">
           New Post
         </RouterLink>
 			</div>
 		</div>
 	</div>
+
+  <Teleport to="#modal">
+    Content to be rended
+  </Teleport>
 </template>
