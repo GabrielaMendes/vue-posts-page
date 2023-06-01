d6aea72dc7d4a3b0c08f39a3197ae646512f5f9c
Gabriela <gdemelo.mendes@gmail.com>
2023-06-01 11:40:06 -0300

    feature: implemented aauthentication workflow

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,14 @@
 import * as express from "express";
 import * as cors from "cors";
 import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as jsonwebtoken from "jsonwebtoken";
 import { today, thisWeek, thisMonth, Post } from "../posts";
 import { NewUser, User } from "../users";
 
 const app = express();
 app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 const allPosts = [today, thisWeek, thisMonth];
@@ -21,9 +24,31 @@ app.post<{}, {}, Post>("/posts", (req, res) => {
 	res.json(post);
 });
 
+const SECRET = "my-secret";
+const COOKIE = "vuejs-jwt";
+
+function authenticate(id: string, req: express.Request, res: express.Response) {
+	const token = jsonwebtoken.sign({ id }, SECRET, {
+		issuer: "vuejs-course",
+		expiresIn: "30 days",
+	});
+	res.cookie(COOKIE, token, { httpOnly: true });
+}
+
+app.get("/current-user", (req, res) => {
+	try {
+		const token = req.cookies[COOKIE];
+		const result = jsonwebtoken.verify(token, "SECRET") as { id: string };
+		res.json({ id: result.id });
+	} catch (e) {
+		res.status(404).end();
+	}
+});
+
 app.post<{}, {}, NewUser>("/users", (req, res) => {
 	const user: User = { ...req.body, id: (Math.random() * 100000).toFixed() };
 	allUsers.push(user);
+	authenticate(user.id, req, res);
 	const { password, ...rest } = user;
 	res.json(rest);
 });
