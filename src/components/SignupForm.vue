abb284aa26530cb0a6668bc1521799d3c6ccaba1
Gabriela <gdemelo.mendes@gmail.com>
2023-06-01 17:47:45 -0300

    refactor: removing part of signup form to a new reusable component

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,68 +1,18 @@
 <script lang="ts" setup>
-import { ref, computed } from "vue";
-import { validate, lenth, required } from "../validation";
-import { useModal } from "../composables/modal";
 import { NewUser } from "../users";
 import { useUsers } from "../stores/users";
-import FormInput from "./FormInput.vue";
-
-const username = ref("");
-const usernameStatus = computed(() => {
-	return validate(username.value, [required, lenth({ min: 5, max: 10 })]);
-});
-
-const password = ref("");
-const passwordStatus = computed(() => {
-	return validate(password.value, [required, lenth({ min: 8, max: 30 })]);
-});
-
-const isInvalid = computed(() => {
-	return !usernameStatus.value.valid || !passwordStatus.value.valid;
-});
+import { useModal } from "../composables/modal";
+import UserForm from "./UserForm.vue";
 
 const usersStore = useUsers();
 const modal = useModal();
 
-async function handleSubmit() {
-	if (isInvalid.value) {
-		return;
-	}
-
-	const newUser: NewUser = {
-		username: username.value,
-		password: password.value,
-	};
-
-	try {
-		await usersStore.createUser(newUser);
-	} catch (e) {}
-
-	modal.hideModal();
+async function handleSignup(newUser: NewUser) {
+  await usersStore.createUser(newUser);
+  modal.hideModal();
 }
 </script>
 
 <template>
-	<form class="form" @submit.prevent="handleSubmit">
-		<FormInput
-			name="username"
-			type="text"
-			v-model="username"
-			:status="usernameStatus"
-		/>
-		<FormInput
-			name="password"
-			type="password"
-			v-model="password"
-			:status="passwordStatus"
-		/>
-		<button class="button" :disabled="isInvalid">Submit</button>
-	</form>
+  <UserForm @submit="handleSignup" />
 </template>
-
-<style scoped>
-.form {
-	background: white;
-	padding: 30px;
-	margin-top: 50px;
-}
-</style>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+import { validate, lenth, required } from "../validation";
+import { NewUser } from "../users";
+import FormInput from "./FormInput.vue";
+
+const emit = defineEmits<{
+	(event: "submit", payload: NewUser): void;
+}>();
+
+const username = ref("");
+const usernameStatus = computed(() => {
+	return validate(username.value, [required, lenth({ min: 5, max: 10 })]);
+});
+
+const password = ref("");
+const passwordStatus = computed(() => {
+	return validate(password.value, [required, lenth({ min: 8, max: 30 })]);
+});
+
+const isInvalid = computed(() => {
+	return !usernameStatus.value.valid || !passwordStatus.value.valid;
+});
+
+async function handleSubmit() {
+	if (isInvalid.value) {
+		return;
+	}
+
+	const newUser: NewUser = {
+		username: username.value,
+		password: password.value,
+	};
+
+	try {
+		emit("submit", newUser);
+	} catch (e) {}
+}
+</script>
+
+<template>
+	<form class="form" @submit.prevent="handleSubmit">
+		<FormInput
+			name="username"
+			type="text"
+			v-model="username"
+			:status="usernameStatus"
+		/>
+		<FormInput
+			name="password"
+			type="password"
+			v-model="password"
+			:status="passwordStatus"
+		/>
+		<button class="button" :disabled="isInvalid">Submit</button>
+	</form>
+</template>
+
+<style scoped>
+.form {
+	background: white;
+	padding: 30px;
+	margin-top: 50px;
+}
+</style>
