321fe743aa1877377ff3b6f3044e9636d859d94c
Gabriela <gdemelo.mendes@gmail.com>
2023-06-01 16:51:29 -0300

    feature: implemented navbar conditional rendering

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,23 +1,28 @@
 <script lang="ts" setup>
 import { useModal } from "../composables/modal";
+import { useUsers } from "../stores/users";
 import SignupForm from "./SignupForm.vue";
 
-const modal = useModal()
+const modal = useModal();
+const usersStore = useUsers();
 </script>
 
 <template>
 	<div class="navbar">
 		<div class="navbar-end">
-			<div class="buttons">
-        <button class="button" @click="modal.showModal()">Sigh Up</button>
-				<RouterLink :to="{name: 'new'}" class="button">
-          New Post
-        </RouterLink>
+			<div v-if="usersStore.currentUserId" class="buttons">
+				<RouterLink :to="{ name: 'new' }" class="button"> New Post </RouterLink>
+				<button class="button" @click="">Log Out</button>
+			</div>
+
+			<div v-else class="buttons">
+				<button class="button" @click="modal.showModal()">Sign Up</button>
+				<button class="button" @click="modal.showModal()">Sign In</button>
 			</div>
 		</div>
 	</div>
 
-  <Teleport to="#modal">
-    <SignupForm />
-  </Teleport>
+	<Teleport to="#modal">
+		<SignupForm />
+	</Teleport>
 </template>
--- src/stores/users.ts
+++ src/stores/users.ts
@@ -1,7 +1,15 @@
 import { defineStore } from "pinia";
 import { NewUser } from "../users";
 
+interface UserState {
+	currentUserId?: string;
+}
+
 export const useUsers = defineStore("users", {
+	state: (): UserState => ({
+		currentUserId: undefined,
+	}),
+
 	actions: {
 		createUser(newUser: NewUser) {
 			const body = JSON.stringify(newUser);
@@ -13,6 +21,16 @@ export const useUsers = defineStore("users", {
 				},
 				body,
 			});
+		},
+
+		async authenticate() {
+			const res = await window.fetch("/api/current-user", {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			const result = await res.json();
+			this.currentUserId = result.id;
 		},
 	},
 });
